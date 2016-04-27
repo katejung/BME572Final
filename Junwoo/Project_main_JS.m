b01dd7bc7849a21295bbe32413487a8a83507301
gonewkid <gonewkid@gmail.com>
2016-04-27 03:15:59 -0500

    .

--- Junwoo/Project_main_JS.m
+++ Junwoo/Project_main_JS.m
@@ -129,6 +129,7 @@ MAp_train_poststimulus = MAp_poststimulus(1:4,samplerate*30+1:end-poststimulusc)
 MHp_train_poststimulus = MHp_poststimulus(1:4,samplerate*30+1:end-poststimulusc);
 MAp_train_intense = MAp_intense(1:4,:);
 MHp_train_intense = MHp_intense(1:4,:);
+
 dataInterest = MAp_train_poststimulus;
 [peakinterval,validPeakCell, validTroughCell, validPeakLocCell, validTroughLocCell] = extractFeatures_height(dataInterest);
 dataMedian = 0;
@@ -136,6 +137,23 @@ for m = 1:4
     dataMedian = dataMedian + median(abs(dataInterest(m,:))/0.6745);
 end
 dataMedian = dataMedian/4;
+
+peaks = [];
+troughs = [];
+peakT = [];
+troughT = [];
+for i = 1:4
+    peaks = [peaks validPeakCell{i,1}];
+    troughs = [troughs validTroughCell{i,1}];
+    peakT = [peakT validPeakLocCell{i,1}];
+    troughT = [troughT validTroughLocCell{i,1}];
+end
+
+num = size(peaks,2);
+for j = 1:num
+    heights(j) = peaks(j)-troughs(j);
+    widths(j) = troughT(j)-peakT(j);
+end
 
 % overlapping signals are not
 figure
@@ -150,7 +168,7 @@ for i = 1:4
         if peak_trial(j) > 7*dataMedian 
             plot(data_trial(peakTime_trial(j)-19:peakTime_trial(j)+44),'b');
         else
-             plot(data_trial(peakTime_trial(j)-19:peakTime_trial(j)+44),'r');
+            plot(data_trial(peakTime_trial(j)-19:peakTime_trial(j)+44),'r');
         end
         end
     end
@@ -196,6 +214,87 @@ title('sample spike detection')
 
 
 
+%% PCA
+K =2;% number of principal Comonents
+
+X(:,1) = troughs;
+X(:,2) = peaks;
+[n,d]=size(X);
+C=cov(X);
+[V U]=eig(C); %V = eigenvectors U = eigenvalues
+L=diag(U); L', U % principal component directions
+[sortedU, index] = sort(L, 'descend');
+Xproj_PCA = zeros(d,K);
+for j = 1:K
+    Xproj_PCA(:,j) = V(:,index(j));
+end
+Y_trained_PCA = X*Xproj_PCA;
+Xproj_PCA; % the first column is the 1st PC and the second column is the 2nd PC
+
+% calulate 1st PC line
+Xh1_PCA=-35:0.5:5;
+Yh1_PCA = Xh1_PCA.*Xproj_PCA(2,1)./Xproj_PCA(1,1);
+% calculated 2nd PC line
+Xh2_PCA =-35:0.5:5;
+Yh2_PCA = Xh2_PCA.*Xproj_PCA(2,2)./Xproj_PCA(1,2);
+figure
+plot(X(:,1), X(:,2), 'b^', Xh1_PCA,Yh1_PCA,'g-',Xh2_PCA, Yh2_PCA, 'r-')
+legend('data', '1st PCA axis', '2nd PCA axis')
+figure
+
+scatter(Y_trained_PCA(:,1), Y_trained_PCA(:,2), 'bo')
+
+title('Encoded Training Data')
+xlabel(['1st PC [' num2str(Xproj_PCA(1,1)) ',' num2str(Xproj_PCA(2,1)) ']'])
+ylabel(['2nd PC [' num2str(Xproj_PCA(1,2)) ',' num2str(Xproj_PCA(2,2)) ']'])
+
+
+%%
+
+clusterA_peak = [];
+clusterA_trough = [];
+for i = 1:size(Y_trained_PCA,1)
+    I = find(Y_trained_PCA(:,1)>20);
+end
+
+for j = 1:4
+    clearvars matrix I_Apeak I_Bpeak
+    matrix1 = validTroughCell{j,1};
+    matrix2 = validPeakCell{j,1};
+	matrix(:,1) = matrix1;
+    matrix(:,2) = matrix2;
+    peakTime = validPeakLocCell{j,1};
+    pca_trained = matrix*Xproj_PCA;
+    figure
+    scatter(pca_trained(:,1), pca_trained(:,2), 'bo')
+    I_Apeak = find(pca_trained(:,1)>20);
+    I_Bpeak = find(pca_trained(:,1)<=20);
+    Apeak_index{j,1} = I_Apeak;
+    Bpeak_index{j,1} = I_Bpeak;
+end
+
+%% spike alighment according to PCA
+for i = 1:4
+    windowLength = 100;
+    peakTime_trial = validPeakLocCell{i,1};
+    peak_trial = validPeakCell{i,1};
+    ApeakI = Apeak_index{i,1};
+    BpeakI = Bpeak_index{i,1};
+    data_trial = dataInterest(i,:);
+    for j = 1:size(ApeakI,1)
+        peakStart = ApeakI(j);
+         plot(data_trial(peakTime_trial(peakStart)-19:peakTime_trial(peakStart)+44),'b');
+    end
+    for k = 1:size(BpeakI,1)
+        peakStart = BpeakI(k);
+         plot(data_trial(peakTime_trial(peakStart)-19:peakTime_trial(peakStart)+44),'r');
+    end
+    
+    hold on  
+end
+xlabel('time spike at 20 sec')
+ylabel('voltage')
+title('Spike Alignment with PCA classification')
 
 
 
